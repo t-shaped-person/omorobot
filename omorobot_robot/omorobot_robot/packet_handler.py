870b80b3f250063cabb32e0bc488985e571bc3ad
t-shaped-person <t.shaped.person@gmail.com>
2025-09-01 20:14:44 +0900

    20250901

--- omorobot_robot/omorobot_robot/packet_handler.py
+++ omorobot_robot/omorobot_robot/packet_handler.py
@@ -18,31 +18,29 @@ class PacketHandler:
             raise RuntimeError('Serial port open error')
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
-        self.incoming_data = ['ENC', 'GYRO', 'ODO', 'POSE', 'VW']
+        # self.incoming_data = ['ENC', 'POSE']
         self._bat = [0.0, 0.0, 0.0]
         self._enc = [0, 0]
-        self._gyro = [0.0, 0.0, 0.0]
-        self._odo = [0.0, 0.0]
         self._pose = [0.0, 0.0, 0.0]
-        self._vw = [0.0, 0.0]
         self.print('Serial init complete')
 
     def print(self, str_info):
         get_logger('packet_handler').info(str_info)
 
     def try_reconnect(self):
-        if not self.ser.readable():
-            return
-        else:
-            self.ser.close()
+        try:
+            if self.ser.is_open:
+                self.ser.close()
+        except Exception as e:
+            pass
         while not self.ser.is_open:
             try:
                 self.ser.open()
                 self.print(f'reconnect to {self.ser.port}')
                 self.ser.reset_input_buffer()
                 self.ser.reset_output_buffer()
-            except serial.SerialException:
-                self.print('reconnect failed.')
+            except Exception as e:
+                self.print(f'reconnect failed. {e}')
                 time.sleep(1)
     
     def write_data(self, tx_string):
@@ -69,19 +67,13 @@ class PacketHandler:
     def odo_reset(self):
         self.write_data('$cODO,0')
 
-    def vw_command(self, lin_vel, ang_vel):
-        self.write_data('$cVW,{:.0f},{:.0f}'.format(lin_vel, ang_vel))
+    def vw_command(self, lin_vel: float, ang_vel: float) -> None:
+        self.write_data(f'$cVW,{int(round(lin_vel))},{int(round(ang_vel))}')
 
-    def set_periodic_info(self, millisecond):
-        for idx, item in enumerate(self.incoming_data):
-            self.write_data('$cREGI,' + str(idx) + ',' + item)
-            self.print(self.ser.readline().strip())
-            time.sleep(0.001)
-        self.write_data('$cPERI,' + str(millisecond))
-        self.print(self.ser.readline().strip())
+    def start_communication(self):
         self.write_data('$cPEEN,1')
         self.print(self.ser.readline().strip())
-        self.print('set periodic info complete')
+        self.print('serial data receiving...')
 
     def read_packet(self):
         try:
@@ -91,17 +83,11 @@ class PacketHandler:
             # self.print(whole_packet)                                                    # for debugging
             packet = whole_packet.split(',')
             header = packet[0].split('#')[1]
-            if header.startswith('ENC'):                                                # encoder
+            if header.startswith('ENC') and len(packet)==3:                             # encoder
                 self._enc = [int(packet[1]), int(packet[2])]
-            elif header.startswith('GYRO'):                                             # gyro
-                self._gyro = [float(packet[1]), float(packet[2]), float(packet[3])]
-            elif header.startswith('ODO'):                                              # omdometer(mm)
-                self._odo = [float(packet[1]), float(packet[2])]
-            elif header.startswith('POSE'):                                             # roll, pitch, yaw
+            elif header.startswith('POSE') and len(packet)==4:                          # roll, pitch, yaw
                 self._pose = [float(packet[1]), float(packet[2]), float(packet[3])]
-            elif header.startswith('VW'):                                               # lin_vel, ang_vel
-                self._vw = [float(packet[1]), float(packet[2])]
-            elif header.startswith('BAT'):
+            elif header.startswith('BAT') and len(packet)==4:
                 self._bat = [float(packet[1]), float(packet[2]), float(packet[3])]
         except Exception as e:
             self.print(f'Serial read error: {e}')
--- omorobot_robot/omorobot_robot/robot_control.py
+++ omorobot_robot/omorobot_robot/robot_control.py
@@ -1,53 +1,49 @@
 import os
 import math
 import rclpy
-import transforms3d
 from rclpy.node import Node
 from .packet_handler import *
-from rclpy.qos import QoSProfile
 from nav_msgs.msg import Odometry
-from rclpy.parameter import Parameter
-from sensor_msgs.msg import Imu, JointState
 from tf2_ros import TransformBroadcaster
+from sensor_msgs.msg import Imu, JointState
+from rclpy.qos import QoSProfile, ReliabilityPolicy
 from geometry_msgs.msg import TransformStamped, Twist, Pose
 
 class RobotControl(Node):
     def __init__(self):
         super().__init__('robot_control')
-        self.declare_parameters(namespace='', parameters=[
-            ('port.name', Parameter.Type.STRING),
-            ('port.baudrate', Parameter.Type.INTEGER),
-            ('wheel.separation', Parameter.Type.DOUBLE),
-            ('wheel.radius', Parameter.Type.DOUBLE),
-            ('motor.gear_ratio', Parameter.Type.DOUBLE),
-            ('motor.max_lin_vel', Parameter.Type.DOUBLE),
-            ('motor.max_ang_vel', Parameter.Type.DOUBLE),
-            ('sensor.enc_pulse', Parameter.Type.DOUBLE),
-            ('sensor.old_enc_pulse', Parameter.Type.DOUBLE),
-            ('sensor.new_enc_pulse', Parameter.Type.DOUBLE),
-            ('sensor.use_imu', Parameter.Type.BOOL)]
-        )
+        self.declare_parameter('port.name', '/dev/ttyMCU')
+        self.declare_parameter('port.baudrate', 115200)
+        self.declare_parameter('wheel.separation', 0.18)
+        self.declare_parameter('wheel.radius', 0.034)
+        self.declare_parameter('motor.gear_ratio', 34.0)
+        self.declare_parameter('motor.max_lin_vel', 0.3)
+        self.declare_parameter('motor.max_ang_vel', 0.5)
+        self.declare_parameter('sensor.enc_pulse', 4000.0)
+        self.declare_parameter('sensor.old_enc_pulse', 44.0)
+        self.declare_parameter('sensor.new_enc_pulse', 1440.0)
+        self.declare_parameter('sensor.use_imu', False)
         
-        port = self.get_parameter_or('port.name', Parameter('port.name', Parameter.Type.STRING, '/dev/ttyMCU')).get_parameter_value().string_value
-        baudrate = self.get_parameter_or('port.baudrate', Parameter('port.baudrate', Parameter.Type.INTEGER, 115200)).get_parameter_value().integer_value
-        self.wheel_separation = self.get_parameter_or('wheel.separation', Parameter('wheel.separation', Parameter.Type.DOUBLE, 0.17)).get_parameter_value().double_value
-        self.wheel_radius = self.get_parameter_or('wheel.radius', Parameter('wheel.radius', Parameter.Type.DOUBLE, 0.034)).get_parameter_value().double_value
-        self.motor_gear_ratio = self.get_parameter_or('motor.gear_ratio', Parameter('motor.gear_ratio', Parameter.Type.DOUBLE, 21.3)).get_parameter_value().double_value
-        self.motor_max_lin_vel = self.get_parameter_or('motor.max_lin_vel', Parameter('motor.max_lin_vel', Parameter.Type.DOUBLE, 2.0)).get_parameter_value().double_value
-        self.motor_max_ang_vel = self.get_parameter_or('motor.max_ang_vel', Parameter('motor.max_ang_vel', Parameter.Type.DOUBLE, 2.5)).get_parameter_value().double_value
+        port = self.get_parameter('port.name').value
+        baudrate = self.get_parameter('port.baudrate').value
+        self.wheel_separation = self.get_parameter('wheel.separation').value
+        self.wheel_radius = self.get_parameter('wheel.radius').value
+        self.motor_gear_ratio = self.get_parameter('motor.gear_ratio').value
+        self.motor_max_lin_vel = self.get_parameter('motor.max_lin_vel').value
+        self.motor_max_ang_vel = self.get_parameter('motor.max_ang_vel').value
         robot_model = os.getenv('ROBOT_MODEL', 'R2MINI')
         self.print(f'ROBOT_MODEL: {robot_model}')
         motor_model = os.getenv('MOTOR_MODEL', 'NEW')
         self.print(f'MOTOR_MODEL: {motor_model}')
         if robot_model == 'R2MINI':
             if motor_model == 'NEW':
-                self.enc_pulse = self.get_parameter_or('sensor.new_enc_pulse', Parameter('sensor.new_enc_pulse', Parameter.Type.DOUBLE, 1440.0)).get_parameter_value().double_value
+                self.enc_pulse = self.get_parameter('sensor.new_enc_pulse').value
             elif motor_model == 'OLD':
-                self.enc_pulse = self.get_parameter_or('sensor.old_enc_pulse', Parameter('sensor.old_enc_pulse', Parameter.Type.DOUBLE, 44.0)).get_parameter_value().double_value
+                self.enc_pulse = self.get_parameter('sensor.old_enc_pulse').value
         else: # R2, DONKEYBOTI, ETC
-            self.enc_pulse = self.get_parameter_or('sensor.enc_pulse', Parameter('sensor.enc_pulse', Parameter.Type.DOUBLE, 4000.0)).get_parameter_value().double_value
-        self.distance_per_pulse = 2 * math.pi * self.wheel_radius / self.enc_pulse / self.motor_gear_ratio
-        self.use_imu = self.get_parameter_or('sensor.use_imu', Parameter('sensor.use_imu', Parameter.Type.BOOL, False)).get_parameter_value().bool_value
+            self.enc_pulse = self.get_parameter('sensor.enc_pulse').value
+        self.distance_per_pulse = 2.0 * math.pi * self.wheel_radius / self.enc_pulse / self.motor_gear_ratio
+        self.use_imu = self.get_parameter('sensor.use_imu').value
         self.print(f'{"port.name":20} {port}')
         self.print(f'{"port.baudrate":20} {baudrate}')
         self.print(f'{"wheel.separation":20} {self.wheel_separation}')
@@ -60,24 +56,28 @@ class RobotControl(Node):
         self.print(f'{"sensor.use_imu":20} {self.use_imu}')
 
         self.ph = PacketHandler(port, baudrate)
-        self.ph.set_periodic_info(50)
+        self.ph.start_communication()
 
-        qos_profile = QoSProfile(depth=10)
-        self.sub_cmd_vel = self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_callback, qos_profile)   # linear/angular velocity
+        qos_profile = QoSProfile(depth=5)
+        qos_profile.reliability = ReliabilityPolicy.BEST_EFFORT
+        self.sub_cmd_vel = self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_callback, 10)            # linear/angular velocity
         self.pub_joint_state = self.create_publisher(JointState, 'joint_states', qos_profile)               # axis position/velocity
-        self.pub_odom = self.create_publisher(Odometry, 'odom', qos_profile)                                # linear/angular velocity, position/orientation
+        self.pub_odom = self.create_publisher(Odometry, 'odom', 10)                                         # linear/angular velocity, position/orientation
         if self.use_imu:
             self.pub_pose = self.create_publisher(Pose, 'pose', qos_profile)
         self.tf_bc = TransformBroadcaster(self)
-        self.timer_2ms = self.create_timer(0.002, self.update_encoder)                                      # 2ms timer
+        self.timer_5ms = self.create_timer(0.005, self.update_encoder)                                      # 5ms timer
         self.timer_10ms = self.create_timer(0.01, self.update_robot)                                        # 10ms timer
 
         self.ph.read_packet()
-        self.enc_lh, self.enc_rh = 0, 0
+        self.enc_lh, self.enc_rh = None, None
         self.enc_lh_pre, self.enc_rh_pre = self.ph._enc[0], self.ph._enc[1]
+        self.delta_lh, self.delta_rh = None, None
         self.x, self.y, self.theta = 0.0, 0.0, 0.0
         self.lin_vel, self.ang_vel = 0.0, 0.0
+        self.time_now = None
         self.time_pre = self.get_clock().now()
+        self.wheel_lh_pos, self.wheel_rh_pos = 0.0, 0.0
 
     def print(self, str_info):
         self.get_logger().info(str_info)
@@ -85,7 +85,7 @@ class RobotControl(Node):
     def cmd_vel_callback(self, msg):
         v = max(-self.motor_max_lin_vel, min(self.motor_max_lin_vel, msg.linear.x))
         w = max(-self.motor_max_ang_vel, min(self.motor_max_ang_vel, msg.angular.z))
-        self.ph.vw_command(v * 1000, w * 1000)                                                              # form m/s & rad/s to mm/s & mrad/s
+        self.ph.vw_command(v * 1000.0, w * 1000.0)                                                          # form m/s & rad/s to mm/s & mrad/s
 
     def update_encoder(self):
         self.ph.read_packet()
@@ -111,21 +111,36 @@ class RobotControl(Node):
         self.theta += delta_theta
         self.lin_vel = delta_s / dt
         self.ang_vel = delta_theta / dt
-        q = transforms3d.euler.euler2quat(0, 0, self.theta)
         odometry = Odometry()
         odometry.header.frame_id = "odom"
         odometry.header.stamp = time_now.to_msg()
         odometry.pose.pose.position.x = self.x
         odometry.pose.pose.position.y = self.y
         odometry.pose.pose.position.z = 0.0
-        odometry.pose.pose.orientation.x = q[1]
-        odometry.pose.pose.orientation.y = q[2]
-        odometry.pose.pose.orientation.z = q[3]
-        odometry.pose.pose.orientation.w = q[0]
+        odometry.pose.pose.orientation.x = 0.0
+        odometry.pose.pose.orientation.y = 0.0
+        odometry.pose.pose.orientation.z = math.sin(self.theta/2.0)
+        odometry.pose.pose.orientation.w = math.cos(self.theta/2.0)
+        odometry.pose.covariance = [
+            0.01,   0.0,    0.0,    0.0,    0.0,    0.0, # +-10cm
+            0.0,    0.01,   0.0,    0.0,    0.0,    0.0,
+            0.0,    0.0,    1e3,    0.0,    0.0,    0.0,
+            0.0,    0.0,    0.0,    1e3,    0.0,    0.0,
+            0.0,    0.0,    0.0,    0.0,    1e3,    0.0,
+            0.0,    0.0,    0.0,    0.0,    0.0,    0.05 # +-12degree
+        ]
         odometry.child_frame_id = "base_footprint"
         odometry.twist.twist.linear.x = self.lin_vel
         odometry.twist.twist.linear.y = 0.0
         odometry.twist.twist.angular.z = self.ang_vel
+        odometry.twist.covariance = [
+            0.1,    0.0,    0.0,    0.0,    0.0,    0.0, # +-0.3m/s
+            0.0,    0.1,    0.0,    0.0,    0.0,    0.0,
+            0.0,    0.0,    1e3,    0.0,    0.0,    0.0,
+            0.0,    0.0,    0.0,    1e3,    0.0,    0.0,
+            0.0,    0.0,    0.0,    0.0,    1e3,    0.0,
+            0.0,    0.0,    0.0,    0.0,    0.0,    0.2 # +-0.45rad/s
+        ]
         self.pub_odom.publish(odometry)
         odom_tf = TransformStamped()
         odom_tf.header.stamp = odometry.header.stamp
@@ -138,15 +153,15 @@ class RobotControl(Node):
         self.tf_bc.sendTransform(odom_tf)
 
     def update_jointstate(self, time_now):
-        wheel_lh_pos = self.delta_lh * self.distance_per_pulse / self.wheel_radius                          # wheel angle (rad)
-        wheel_rh_pos = self.delta_rh * self.distance_per_pulse / self.wheel_radius
+        self.wheel_lh_pos += self.delta_lh * self.distance_per_pulse / self.wheel_radius                    # wheel angle (rad)
+        self.wheel_rh_pos += self.delta_rh * self.distance_per_pulse / self.wheel_radius
         wheel_lh_vel = (self.lin_vel - (self.wheel_separation / 2.0) * self.ang_vel) / self.wheel_radius    # wheel angular velocity (rad/s)
         wheel_rh_vel = (self.lin_vel + (self.wheel_separation / 2.0) * self.ang_vel) / self.wheel_radius
         jointstate = JointState()
         jointstate.header.frame_id = "base_link"
         jointstate.header.stamp = time_now.to_msg()
         jointstate.name = ['wheel_left_joint', 'wheel_right_joint']
-        jointstate.position = [wheel_lh_pos, wheel_rh_pos]
+        jointstate.position = [self.wheel_lh_pos, self.wheel_rh_pos]
         jointstate.velocity = [wheel_lh_vel, wheel_rh_vel]
         jointstate.effort = []
         self.pub_joint_state.publish(jointstate)
@@ -166,6 +181,8 @@ def main(args=None):
     except Exception as e:
         node.get_logger().error(f'Exception occurred: {e}')
     finally:
+        node.destroy_timer(node.timer_5ms)
+        node.destroy_timer(node.timer_10ms)
         node.ph.close_port()
         node.destroy_node()
         rclpy.shutdown()
